2cca3413d93bbef28bf4a2090db40e8a9e633c98
Nikhil Mehra <85126935+hackwithnick@users.noreply.github.com>
2023-12-28 16:30:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
+const BASE_URL = "https://g.co/finance/";
 
 const dropDownSelect = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
@@ -33,41 +32,15 @@ for (let select of dropDownSelect) {
     updateFlag(evt.target);
   });
 }
-// for (let select of dropDownSelect) {
-//   for (let currencyCode in countryList) {
-//     let newOption = document.createElement("option");
-//     newOption.innerText = currencyCode;
-//     newOption.value = currencyCode;
-//     if (select.name === "from" && currencyCode === "USD") {
-//       newOption.selected = "selected";
-//     }
-//     if (select.name === "to" && currencyCode === "INR") {
-//       newOption.selected = "selected";
-//     }
-//     select.append(newOption);
-//   }
-//   select.addEventListener("change", (evt) => {
-//     updateFlag(evt.target);
-//   });
-// }
 
 const updateFlag = (element) => {
   let currencyCode = element.value;
-  // console.log(currencyCode);
   let countryCode = currencyListName[currencyCode].code;
-  // console.log(countryCode);
   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
   let img = element.parentElement.querySelector("img");
   img.src = newSrc;
 };
-// const updateFlag = (element) => {
-//   let currencyCode = element.value;
-//   console.log(currencyCode);
-//   let countryCode = countryList[currencyCode];
-//   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-//   let img = element.parentElement.querySelector("img");
-//   img.src = newSrc;
-// };
+
 
 btn.addEventListener("click", (evt) => {
   evt.preventDefault();
@@ -92,7 +65,7 @@ const updateExchangeRate = async () => {
     amount.value = "1";
   }
   // console.log(fromCurrency.value, toCurrency.value);
-  const URL = `${BASE_URL}${fromCurrency.value.toLowerCase()}/${toCurrency.value.toLowerCase()}.json`;
+  const URL = `${BASE_URL}${fromCurrency.value}-${toCurrency.value}`;
   let response = await fetch(URL);
   let data = await response.json();
   let rate = data[toCurrency.value.toLowerCase()];
