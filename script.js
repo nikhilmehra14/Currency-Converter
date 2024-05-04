a06c4e1a13798139b5ca9b27fa921289cdb3228b
Nikhil Mehra <85126935+nikhilmehra14@users.noreply.github.com>
2024-05-04 14:04:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
-
+//const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
+const Base_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/";
 const dropDownSelect = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 let fromCurrency = document.querySelector(".from select");
